31eace82987af1ec3f0da95d25d3596c7facee44
Maurício Taffarel <contact@taffarel.tech>
2024-08-05 21:45:49 -0300

    Atualização de links

--- config.js
+++ config.js
@@ -11,23 +11,22 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'Maurício',
+  name: "Maurício",
   imageBackground: false,
   openInNewTab: true,
 
   // Greetings
-  greetingMorning: 'Bom dia!',
-  greetingAfternoon: 'Boa tarde,',
-  greetingEvening: 'Boa noite,',
-  greetingNight: 'Vá dormir!',
+  greetingMorning: "Bom dia!",
+  greetingAfternoon: "Boa tarde,",
+  greetingEvening: "Boa noite,",
+  greetingNight: "Vá dormir!",
 
   // Weather
-  weatherKey: '1ca95b56825acca80465b075f7fc3dfe',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C',
-  weatherLatitude: '-23.207339',
-  weatherLongitude: '-45.8767399',
-  
+  weatherKey: "1ca95b56825acca80465b075f7fc3dfe",
+  weatherIcons: "OneDark", // 'Nord', 'Dark', 'White'
+  weatherUnit: "C",
+  weatherLatitude: "-23.207339",
+  weatherLongitude: "-45.8767399",
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -36,40 +35,40 @@ const CONFIG = {
   // Links
   cards: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/",
     },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.google.com/mail/u/0/',
+      id: "2",
+      name: "Mail",
+      icon: "mail",
+      link: "https://mail.google.com/mail/u/0/",
     },
     {
-      id: '3',
-      name: 'Trello',
-      icon: 'trello',
-      link: 'https://trello.com/b/I0P7nWQz/corres',
+      id: "3",
+      name: "Trello",
+      icon: "trello",
+      link: "https://trello.com/b/I0P7nWQz/corres",
     },
     {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '5',
-      name: 'UFBA',
-      icon: 'book',
-      link: 'https://ava.ufba.br/',
+      id: "5",
+      name: "Hackmd",
+      icon: "list",
+      link: "https://hackmd.io/cr7B53OAQceHvrCKetdgsg?both",
     },
     {
-      id: '6',
-      name: 'Youtube',
-      icon: 'youtube',
-      link: 'https://www.youtube.com/',
+      id: "6",
+      name: "Finanças",
+      icon: "table",
+      link: "https://docs.google.com/spreadsheets/d/1v-nVngPoDarPBnZV_4VlrSNiRJgpde9gamVSgSVFI1o/edit?gid=633528333#gid=633528333",
     },
   ],
 
@@ -78,45 +77,45 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: "music",
+  secondListIcon: "globe",
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Spotify",
+        link: "https://open.spotify.com",
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Castbox",
+        link: "https://castbox.fm",
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Youtube Music",
+        link: "https://music.youtube.com/",
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Lyrics Training",
+        link: "https://lyricstraining.com/",
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: "Duolingo",
+        link: "https://www.duolingo.com/",
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: "Tandem",
+        link: "https://app.tandem.net",
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: "Whatsapp",
+        link: "https://web.whatsapp.com/",
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: "Telegram",
+        link: "https://webk.telegram.org",
       },
     ],
   },
